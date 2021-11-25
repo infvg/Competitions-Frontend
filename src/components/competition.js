c081bda0f3521631c33edd5dec882a3cb14902b4
inf <ialhazmim@gmail.com>
2021-11-25 15:37:56 +0300

    Template

---
+++ src/components/competition.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+class Competition extends React.Component {
+    render() {
+        return ;
+      }
+}
+
+export default Competition
---
+++ src/components/student.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+class Student extends React.Component {
+    render() {
+        return ;
+      }
+}
+
+export default Student
