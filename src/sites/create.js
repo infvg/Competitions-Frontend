6a0c87520de44e0db21c63776d0275c965af71a0
inf <ialhazmim@gmail.com>
2021-12-14 21:34:45 +0300

    Fixed and working

--- src/sites/create.js
+++ src/sites/create.js
@@ -2,7 +2,57 @@ import { Box, Button, Checkbox, Container, FormControlLabel, Grid, TextField } f
 import { useRef, useState } from 'react';
 import '../css/create.css';
 import Header from './headerBar'
-import CompForm from '../components/CompForm'
+
+function CompForm(props) {
+  return (
+
+      <Container>
+          <div>
+              <TextField required
+                  name="name"
+                  label="Competition Name"
+                  variant="filled"
+                  value={props.data === undefined ? "" : props.data.name}
+
+              />
+              <TextField required
+                  name="link"
+                  label="Competition Link"
+                  variant="filled"
+                  value={props.data === undefined ? "" : props.data.link}
+
+              />
+              <TextField required
+                  name="date"
+                  label="Competition Date"
+                  variant="filled"
+                  value={props.data === undefined ? "" : props.data.date}
+                  type="date"
+              />
+              {getFromCompetition(props.data)}
+          </div>
+
+      </Container>
+
+  );
+
+}
+function getFromCompetition(props) {
+  if (props === undefined)
+      return <TeamForm data={[[{}, <StudentForm data={[{ name: "", major: "", stId: "" },]} />]]} />;
+
+  let comp = []
+  props.teams.array.forEach(team => {
+      let students = [];
+
+      props.teams.array.forEach(student => {
+          students.push({ name: student.name, major: student.major, stId: student.stId })
+      })
+      comp.push([{ name: team.name, winner: team.iswinner }, <StudentForm data={students} />])
+  })
+
+  return <TeamForm data={comp} />
+}
 
 let counter = 0;
 function TeamForm(props){
