bb93643e8d5137683811fb23935618fe4b19ba9a
inf <ialhazmim@gmail.com>
2021-12-17 20:45:08 +0300

    Track feature done

--- src/sites/ViewCompetitions.js
+++ src/sites/ViewCompetitions.js
@@ -19,98 +19,7 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import StudentRow from "../components/StudentRow"
 import TeamRow from "../components/TeamRow";
 import { useSnackbar } from "notistack";
-
-function allStudents(props){
-  let OverOne = false;
-    props.teams.map(team => {
-      if(team.students.length > 1)
-        OverOne = true;
-
-  });
-    return OverOne;
-}
-
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-  
-  return (
-    <React.Fragment>
-      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right"><a href ={row.link}>{row.link}</a></TableCell>
-        <TableCell align="right">{row.date}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              { allStudents(row) ?
-              
-              (<div>
-                <Typography variant="h6" gutterBottom component="div">
-                Teams
-              </Typography>
-                {row.teams.map((team) => (
-                <TeamRow key={team.name} row={team} comp={row.name}/>
-              ))}</div>) : (<div>
-                <Typography variant="h6" gutterBottom component="div">
-                Students
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                      <TableCell>Student ID</TableCell>
-                      <TableCell align="right">Major</TableCell>
-                    </TableRow>
-                  </TableHead>
-                {row.teams.map((team) => (
-                <StudentRow key={team.name} row={team} comp={row.name}/>
-                
-              ))}
-              </Table>
-              </div>)
-              }
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
-Row.propTypes = {
-  row: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    teams: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        winner: PropTypes.bool,
-        students: PropTypes.arrayOf(
-          PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            stId: PropTypes.string.isRequired,
-            major: PropTypes.string.isRequired
-          })
-        )
-      })
-    ).isRequired
-  }).isRequired
-};
+import CompRow from "../components/CompRow";
 
 export default function ViewCompetitions() {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
@@ -179,7 +88,7 @@ React.useEffect(() => {
         </TableHead>
         <TableBody>
           {top.map((row) => (
-            <Row key={row.name} row={row} />
+            <CompRow key={row.name} row={row} />
           ))}
         </TableBody>
       </Table>
--- src/sites/track.js
+++ src/sites/track.js
@@ -17,106 +17,16 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import StudentRow from "../components/StudentRow"
 import TeamRow from "../components/TeamRow";
+import CompRow from "../components/CompRow";
+import Timer from "../components/Timer";
 
 
 const drawerWidth = 240;
-function allStudents(props){
-    let OverOne = false;
-      props.teams.map(team => {
-        if(team.students.length > 1)
-          OverOne = true;
-  
-    });
-      return OverOne;
-  }
-  
-  function Row(props) {
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-    
-    return (
-      <React.Fragment>
-        <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-          <TableCell>
-            <IconButton
-              aria-label="expand row"
-              size="small"
-              onClick={() => setOpen(!open)}
-            >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {row.name}
-          </TableCell>
-          <TableCell align="right"><a href ={row.link}>{row.link}</a></TableCell>
-          <TableCell align="right">{row.date}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
-                { allStudents(row) ?
-                
-                (<div>
-                  <Typography variant="h6" gutterBottom component="div">
-                  Teams
-                </Typography>
-                  {row.teams.map((team) => (
-                  <TeamRow key={team.name} row={team} comp={row.name}/>
-                ))}</div>) : (<div>
-                  <Typography variant="h6" gutterBottom component="div">
-                  Students
-                </Typography>
-                <Table size="small" aria-label="purchases">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Student ID</TableCell>
-                        <TableCell align="right">Major</TableCell>
-                      </TableRow>
-                    </TableHead>
-                  {row.teams.map((team) => (
-                  <StudentRow key={team.name} row={team} comp={row.name}/>
-                  
-                ))}
-                </Table>
-                </div>)
-                }
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    );
-  }
-  Row.propTypes = {
-    row: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-      teams: PropTypes.arrayOf(
-        PropTypes.shape({
-          name: PropTypes.string.isRequired,
-          winner: PropTypes.bool,
-          students: PropTypes.arrayOf(
-            PropTypes.shape({
-              name: PropTypes.string.isRequired,
-              stId: PropTypes.string.isRequired,
-              major: PropTypes.string.isRequired
-            })
-          )
-        })
-      ).isRequired
-    }).isRequired
-  };
 
 function Track() {
 
   
-    const [data, setData] = useState([]);
-    const [delBut, setDelete] = useState(false);
+    const [data, setData] = useState([]); 
     const [competition, setCompetition] = useState('');
     const [form, setForm] = useState()
     const getData = () => {  
@@ -158,9 +68,9 @@ function Track() {
   }
   React.useEffect(()=>{
     if(competition !== ''){
-        console.log(swapday(getCompFromName(competition).date))
       setForm(<div>
-          <Row key={competition} row={getCompFromName(competition)} /></div>)
+          <Timer date={getCompFromName(competition).date} />
+          <CompRow key={competition} row={getCompFromName(competition)} /></div>)
     }
   },[competition]);
 
